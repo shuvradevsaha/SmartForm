3ccd6f760b539019e1b56d975d850dae5e4aa18a
Shuvra Dev Saha <35439767+shuvradevsaha@users.noreply.github.com>
2018-02-14 09:25:58 +0600

    Add files via upload

--- smartForm.js
+++ smartForm.js
@@ -1,39 +1,61 @@
 // Your jQuery goes here
 
-var userData = {};
-name: '',
-email:'',
-html: {likes:[], dislikes:[]},
-css: {likes:[], dislikes:[]},
-js: {likes:[], dislikes:[]},
-strength: { css:'', js:'', html :''},
-currentQuestion: '#welcome'
+var userData = {
+
+name :'',
+email : '',
+html : {likes :[], dislikes:[]},
+css : {likes :[], dislikes:[]} ,
+js : {likes :[], dislikes:[]} ,
+strength: {css: '', js: '', html: ''} ,
+currentQuestion : '#welcome'
+
+
 };
 
-
-$('#start').click(function(){
-	console.log("start");
-	$('#welcome').hide();
-	$('#q1').show();
-});
-
-$('name').change(funtion(event)
+if(localStorage.getItem('userData'))
 {
-	//
-	console.log($(#name).val());
+  userData= JSON.parse(localStorage.getItem('userData'));
 
-});
-$('#q1next').click(function)(){
-	if($('#name').val() && $('#email').val()){
-userData.name = $('#name').val();
-userData.email = $('#email').val();
-$('#welcome').hide();
-$('#q1').hide();
-$('#q2').show();
+  $('#welcome').hide();
+  $(userData.currentQuestion).show();
+  $('#name').val(userData.name);
+  $('#email').val(userData.email);
 }
 
-else {
-	alert('Please enter your name & email');
-	
+else
+{
+  localStorage.setItem('userData', JSON.stringify(userData));
+}
+
+$('#start').click(function()
+{
+  console.log("start");
+  $('#welcome').hide();
+  $('#q1').show();
+  userData.currentQuestion="#q1";
+  localStorage.setItem('userData' , JSON.stringify(userData));
+});
+
+$('name').change(function(event)
+{
+  //console.log(event);
+  console.log($('#name').val());
+});
+
+$('#next').click(function()
+{
+  if($('#name').val() && $('#email').val())
+  {
+  userData.name= $('name').val();
+  userData.email= $('email').val();
+  $('#welcome').hide();
+  $('#q1').hide();
+  $('#q2').show();
+}
+
+else
+{
+  alert('Please Enter Name & email');
 }
 });
